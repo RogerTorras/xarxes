b283a4a680924ec278ffd4b01ab4fa1736069a2c
Roger <rogert973@gmail.com>
2020-03-10 10:10:49 +0100

    last

--- trivial_torrent/src/pong.c
+++ trivial_torrent/src/pong.c
@@ -49,9 +49,9 @@ int main(int argc, char **argv) {
 	{
 		n = recvfrom(sockfd,(char *) buffer,MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
 		buffer[n] = '\0';
-		sendto(sockfd,(char *) buffer,strlen(buffer), MSG_CONFIRM, (struct sockaddr *) &cliaddr, &len);
+		sendto(sockfd,(char *) buffer,strlen(buffer)+1, MSG_CONFIRM, (struct sockaddr *) &cliaddr, len);
 	}
-
+	close(sockfd);
 	// TODO: some socket stuff here
 
 	return 0;
